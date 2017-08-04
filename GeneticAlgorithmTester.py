e998bf8ec2e2771473e5edcd16a8939ea1d376e5
Zachary Kastl <zkastl8@gmail.com>
2017-08-04 16:03:48 -0500

    refactored algorithm

--- GeneticAlgorithmTester.py
+++ GeneticAlgorithmTester.py
@@ -8,9 +8,10 @@ from copy import deepcopy
 
 '''
 
+
 class Guest:
 
-    def __init__(self, guest_number, first_name, last_name, same_table=[], not_same_table=[], table_number=0):
+    def __init__(self, guest_number, first_name, last_name, same_table, not_same_table, table_number=0):
 
         # Guest's Name
         self.first_name = first_name
@@ -19,30 +20,28 @@ class Guest:
         self.last_name = last_name
 
         # guest's number
-        self.guest_number = guest_number
+        self.guest_number = int(guest_number)
 
         # list of guest numbers the guest should be seated near
-        self.same_table = same_table
+        self.same_table = [int(same) for same in same_table if same != '']
 
         # list of guest numbers the guest should NOT be seated near
-        self.not_same_table = not_same_table
+        self.not_same_table = [int(not_same) for not_same in not_same_table if not_same != '']
 
         # table number a guest is seated at
-        self.table_number = table_number
-
+        self.table_number = int(table_number)
 
     def assign_table(self, table_number):
-        self.table_number = table_number
+        self.table_number = int(table_number)
 
         
-'''
-    Class: Table - 
-        Defines the table object
-
-        Tables can add guests, or remove guests
-'''
 class Table:
+    """
+        Class: Table -
+            Defines the table object
 
+            Tables can add guests, or remove guests
+    """
     def __init__(self, table_number, capacity=8, empty_seats=0):
         self.table_number = table_number
         self.seated_guests = []
@@ -66,16 +65,14 @@ class Table:
         return False
 
 
-'''
-    Class: Layout
-        defines the layout of multiple tables
-
-        cannot create tables itself, but stores a list of tables/guests and can
-        evaulate its own fitness
-
-    NOT READY FOR PRIMETIME
-'''
 class Layout:
+    """
+        Class: Layout
+            defines the layout of multiple tables
+
+            cannot create tables itself, but stores a list of tables/guests and can
+            evaluate its own fitness
+    """
 
     # constructor
     def __init__(self, table_list):
@@ -96,10 +93,11 @@ class Layout:
         return guest_list
    
     # Static function that returns a new layout from a guest list: this returns a new Layout object
-    def create_random_table_layout(guest_list=[], capacity=8, empty_seats=0):
+    @staticmethod
+    def create_random_table_layout(guest_list, capacity=8, empty_seats=0):
 
         # Check for invalid parameters
-        if guest_list == None or len(guest_list) == 0:
+        if guest_list is None or len(guest_list) == 0:
             print("Invalid parameters")
             return None
 
@@ -137,55 +135,32 @@ class Layout:
         '''
         evaluating the fitness of a layout means that each guest is examined for its neighbors at the same table
         a guest has people they can and cannot sit next to; the more times the rules are followed, the better the score
-
-        ***
-        FOR NOW: this function relies on the notion that table numbers determine table proximity to each other
-        example: table 1 - table 2: next to each other - good result
-        example: table 4 - table 21 very far away - good result
-        example: enemies(table 5 - table 3) 2 tables away, score is 2
-        ***
-
+       
         HIGHER FITNESS SCORES ARE ALWAYS BETTER
         '''
         # Starting fitness score
-        fitness_score = 0                        
+        self.fitness_score = 0
 
         # I don't like this loop. I'm sure there's a faster way to perform a "where" statement in
         # Python, but since I have to rewrite this in Java anyway, as long as it works: don't care.
         # What I want to do: good_neighbors = self.all_guests.Where(x => guest.same_table.Contains(x.guest_number))  
         for guest in self.get_guests():
 
-            # allocate a list for guest objects related to the guest numbers in a guest's 'same table' attribute
-            good_neighbors = []
-            bad_neighbors = []
+            good_neighbors = [neighbor for neighbor in self.get_guests() if neighbor.guest_number in guest.same_table]
+            bad_neighbors = [neighbor for neighbor in self.get_guests() if neighbor.guest_number in guest.not_same_table]
 
-            for g in self.get_guests():
-                if g.guest_number in guest.same_table:
-                    good_neighbors.append(g)
-                elif g.guest_number in guest.not_same_table:
-                    bad_neighbors.append(g)                    
+            for neighbor in good_neighbors:
+                if guest.table_number == neighbor.table_number:
+                    self.fitness_score += 2
 
-            # How fitness is determined:
-                # determine the proximity of the guest from both its desired neighbors and undesidred
-                # desired:
-                    # score = num_tables - abs(guest_table - neighbor_table)
-                    # score = 20 tables - abs(table 5 - table 6)
-                    # score = 20 - abs(-1)
-                    # score = 19 = 20 - 1
-                # undesired
-                    # score = abs(guest_table - neighbor_table)
-                    # score = abs(table 19 - table 5)
-                    # score = abs(14)
-                    # score = 14
-            for g in good_neighbors:
-                fitness_score += (len(self.table_list) - abs(guest.table_number - g.table_number))
+            for neighbor in bad_neighbors:
+                if guest.table_number != neighbor.table_number:
+                    self.fitness_score += 1
 
-            for b in bad_neighbors:
-                fitness_score += (abs(guest.table_number - b.table_number))
 
-        self.fitness_score = fitness_score
-        return fitness_score
+        return self.fitness_score
 
+    # print out the layout in a pretty form for debugging
     def print_layout(self):
 
         print("***Table Layout***")
@@ -194,48 +169,58 @@ class Layout:
         for table in self.table_list:
             print("    Table {0}:".format(table.table_number))
             for guest in table.seated_guests:
-                print("      {0} {1}".format(guest.first_name, guest.last_name))
+                print("      {0} {1} ({2}) - {3}, {4}".format(guest.first_name, guest.last_name, guest.guest_number, guest.same_table, guest.not_same_table))
+
+    # fully copy the object, creating a complete copy of the object
+    @staticmethod
+    def deepcopy(layout):
+
+        copied_list = deepcopy(layout.table_list)
+        new_layout = Layout(copied_list)
+        new_layout.evaluate_fitness()
+
+        return new_layout
 
 
-'''
-    GA:
-        Primary driver for the genetic algorithm
-        Takes a guest list, table capacity, and the default number of empty seats
-        and creates a evolved table listing based on GA
-
-    returns: best table listing
-'''
 def GA(guests, capacity=8, empty_seats=2):
+    """
+        GA:
+            Primary driver for the genetic algorithm
+            Takes a guest list, table capacity, and the default number of empty seats
+            and creates a evolved table listing based on GA
 
+        returns: best table listing
+    """
     # Parameter check
-    if (guests == None):
+    if guests is None:
         print("Unable to run algorithm, guest list is empty.")
         return None
 
     # DEFAULT GA PARAMETERS; set these values to change how the algorithm works
     POPULATION_SIZE = 100
-    MUTATION_RATE = 0.05
+    MUTATION_RATE = 0.1
     DEATH_RATE = 0.1
-    FITNESS_THRESHOLD = 100000
+    FITNESS_THRESHOLD = 1000
+    MAX_GENERATIONS = 5000
 
     # Init
     population = []
-    temp_list = guests
+    generation = 1
+    max_fitness = 0
 
-    while len(population) == 0 or (member.fitness_value < FITNESS_THRESHOLD for member in population) and False:
+    # Create an initial population by randomly selecting guests
+    # from the guest list. Relationships play no role in this.
+    for genome in range(0, POPULATION_SIZE):
+        # Create a randomized layout of guests at the table
+        layout = Layout.create_random_table_layout(guests, capacity=capacity, empty_seats=empty_seats)
 
-        # Create an initial population by randomly selecting guests
-        # from the guest list. Relationships play no role in this.
-        for genome in range(0, POPULATION_SIZE):
+        # Evaluate the fitness of the layout
+        layout.evaluate_fitness()
 
-            # Create a randomized layout of guests at the table
-            layout = Layout.create_random_table_layout(temp_list)
+        # Add the member to the population
+        population.append(layout)
 
-            # Evalute the fitness of the layout
-            layout.evaluate_fitness()
-
-            # Add the member to the population
-            population.append(layout)
+    while max_fitness < FITNESS_THRESHOLD and generation < MAX_GENERATIONS:
 
         # Randomly select (round-robin) members to breed and create children
         children = breed(population, DEATH_RATE)
@@ -245,7 +230,7 @@ def GA(guests, capacity=8, empty_seats=2):
 
         # Kill off the lowest fitness of the population to make room for the children
         # Sort the population by fitness value from greatest to smallest
-        population.sort(key=lambda layout: layout.fitness_score, reverse=True)
+        population.sort(key=lambda l: l.fitness_score, reverse=True)
 
         # remove the last X members: where X is the amount of children
         del population[-len(children):]
@@ -254,10 +239,12 @@ def GA(guests, capacity=8, empty_seats=2):
         population.extend(children)
 
         # print the details of the most fit member
-        population[0].print_layout()
+        # Repeat until the fitness is over a certain threshold.        
+        population.sort(key=lambda l: l.fitness_score, reverse=True)
+        print('Generation {0} - Max fitness: {1}'.format(generation, population[0].fitness_score))
+        generation += 1
 
-        # REPEAT THIS METHOD until the fitness is over a certain threshold.
-        pass
+    population[0].print_layout()
 
 
 '''
@@ -279,10 +266,10 @@ def breed(population, death_rate=0.1):
     members_to_select = math.ceil(len(population) * death_rate)
     if members_to_select % 2 != 0: members_to_select += 1
 
-    for c in range(members_to_select):
+    for c in range(int(members_to_select)):
         select = None
 
-        while(select is None or select in _selected):
+        while select is None or select in _selected:
             select = roulette_selection(population)
         
         _selected.append(select)
@@ -294,7 +281,7 @@ def breed(population, death_rate=0.1):
         index += 2
 
     # DEBUG to make sure this is working right
-    if (len(_children) != members_to_select / 2):
+    if len(_children) != members_to_select / 2:
         raise ValueError
 
     return _children
@@ -309,8 +296,8 @@ def breed(population, death_rate=0.1):
 def roulette_selection(population):
 
     fitness_values = [p.fitness_score for p in population]
-    sum_of_fitnesses = sum(fitness_values)
-    pick = random.uniform(0,sum_of_fitnesses)
+    sum_of_fitness = sum(fitness_values)
+    pick = random.uniform(0,sum_of_fitness)
     current = 0
     for p in population:
         current += p.fitness_score
@@ -319,77 +306,98 @@ def roulette_selection(population):
 
     return population[-1]
 
-'''
-    crossover:  ***UNFINISHED***
-    swaps portions of the mother and father's lists to create a child
 
-    returns a new genome with the swapped characteristics
-'''
-def crossover(mother, father):
-    
+def crossover(mother: Layout, father: Layout):
+    """
+        swaps portions of the mother and father's lists to create a child
+
+        returns a new genome with the swapped characteristics
+    """
+
     # make a copy of the mother
-    child = Layout(mother)
+    child = Layout.deepcopy(mother)
 
     # get the count of 'chromosomes' to swap
-    chromosomes_to_swap = len(mother.get_guests()) // 2
+    chromosomes_to_swap = len(child.get_guests()) // 2
 
     # for each of the chromosomes we need to swap, select a random 
     # number of a guest, select that guest and swap that spot with
     #  the position of the same guest on the father
+
+    # NOTE: this works primarily because python is *pointer-happy*
+    # i.e changing the data referenced by a label updates the label
+    # this will probably have to change in the java implementation
     for chromosome in range(0, chromosomes_to_swap):
 
-        selected = math.floor(random.uniform(1, len(mother.get_guests()))) + 1
-        selected_guest = [guest for guest in mother.get_guests() if int(guest.guest_number) == selected]
-        selected_guest_father_table_number = [guest for guest in father.get_guests() if int(guest.guest_number) == selected][0].table_number
+        # randomly select a guest number from the list of guests
+        selected = math.floor(random.uniform(1, len(child.get_guests()))) + 1
 
-def mutate(children=[], mutation_rate=0.05):
-    '''
-    mutate:
-    randomly has a chance to mutate one child based on a mutation rate
-    will not always mutate
+        # select the guest in the mother's genome that corresponds to the number pid
+        selected_guest = [guest for guest in child.get_guests() if int(guest.guest_number) == selected][0]
 
-    function(children=[], mutation_rate=0.05) -> Layout list
-    '''
+        # find the table number of the selected guest in the father's layout
+        fathers_guest = [guest for guest in father.get_guests() if int(guest.guest_number) == selected][0]
+
+        # find the first guest in the mother's layout that is at the table linked to by the father's layout
+        swapped_guest = [guest for guest in child.get_guests() if int(guest.table_number) == fathers_guest.table_number][0]
+
+        # swap the table numbers of the two selected guests
+        backup_guest = selected_guest
+        selected_guest = swapped_guest
+        swapped_guest = backup_guest
+
+    child.evaluate_fitness()
+    return child
+
+
+def mutate(children, mutation_rate=0.05):
 
     # Calculate if the function will mutate a child
-    if (random.random() > mutation_rate):
+    if random.random() > mutation_rate:
         return
     
     # Randomly shuffle the children and apply the mutation to the last element
     random.shuffle(children)
-    children[-1].mutate_genome()
+
+    # mutate
+    mutate_genome(children[-1])
 
 
-'''
-    mutate_genome:
-    Provides details on how to mutate a genome
-    
-    returns the mutated genome
-'''
-'''
-    ***UNFINISHED***
-'''
 def mutate_genome(genome):
-    
+    """
+        mutate_genome:
+        Provides details on how to mutate a genome
+
+        returns the mutated genome
+    """
+
     # randomly select two guests and swap their seats
-    selected_guest_a = genome.guests()[random.uniform(0, len(genome.guests()))]
+    selected_guest_a = genome.get_guests()[int(random.uniform(0, len(genome.get_guests())))]
     selected_guest_b = None
 
+    # verify that the selected b guest is not the same guest
     while selected_guest_b is None or selected_guest_b is selected_guest_a:
-        selected_guest_b = genome.guests()[random.uniform(0, len(genome.guests()))]
+        selected_guest_b = genome.get_guests()[int(random.uniform(0, len(genome.get_guests())))]
 
-    pass
+    # swap the table numbers of the two guests
+    table_number = selected_guest_a.table_number
+    selected_guest_a.table_number = selected_guest_b.table_number
+    selected_guest_b.table_number = table_number
+
+    # swap the guests
+    selected_backup = selected_guest_a
+    selected_guest_a = selected_guest_b
+    selected_guest_b = selected_backup
 
 
-'''
-    create_guests:
-        Creates a list of guest objects formatted from the sample data
-        These guests are not assigned to a table
-
-    returns: a list of guest objects
-'''
 def create_guests(filename):
+    """
+        create_guests:
+            Creates a list of guest objects formatted from the sample data
+            These guests are not assigned to a table
 
+        returns: a list of guest objects
+    """
     guests = []
 
     if filename != '':
@@ -419,6 +427,6 @@ def create_guests(filename):
 if __name__ == '__main__':
 
     CSV_FILE = 'seating_sample.csv'
-    guests = create_guests(CSV_FILE)      
+    csv_guests = create_guests(CSV_FILE)
     
-    GA(guests)
+    GA(csv_guests)
